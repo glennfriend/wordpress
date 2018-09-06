9dc68fa8b63207b89697d97420973d6116a08c02
glenn <glenn.profile@gmail.com>
2018-09-06 09:55:01 +0800

    custom plugins setting links

--- content/plugins/onramp-woo-custom-emails-plus/App/Feature/Settings/Settings.php
+++ content/plugins/onramp-woo-custom-emails-plus/App/Feature/Settings/Settings.php
@@ -23,21 +23,44 @@ class Settings extends Controller
         $this->pageId    = $this->provider->key . 'settings';
         $this->saveName  = null;    // save to database
 
-        //
+        // wordpress form 的基本框架
         $this->tabFocus = $_GET['tab'] ?? $defaultTab;
         if (! array_key_exists($this->tabFocus, $this->tabs)) {
             $this->tabFocus = array_keys($this->tabs)[0];
         }
-
         add_action('admin_menu', [$this, 'settings_page']);
 
 
-        //
+        // 每一頁 tab 的內容由一個 class 來管理, 並且有各自的儲存參數
         $className = $this->tabs[$this->tabFocus];
         $this->saveName = $this->provider->key . $this->tabFocus;
         $class = new $className($this->pageId, $this->saveName);
-
         add_action('admin_init', [$class, 'page']);
+
+
+        // plugin setting links
+        add_filter('plugin_action_links_' . plugin_basename($this->provider->file), [$this, 'plugin_action_links']);
+    }
+
+    /**
+     * Adds plugin action links
+     *
+     * @return array
+     */
+    public function plugin_action_links($links)
+    {
+        $pageId = $this->pageId;
+        $tab = $this->tabFocus;
+        $settingLink = admin_url('admin.php?page=' . $pageId . '&tab=' . $tab);
+
+        $pageId = $this->pageId;
+        $docLink = admin_url('admin.php?page=' . $pageId . '&tab=document');
+
+        $pluginLinks = [
+            '<a href="' . $settingLink .    '">' . __('Settings')   . '</a>',
+            '<a href="' . $docLink .        '">' . __('Doc')        . '</a>',
+        ];
+        return array_merge($pluginLinks, $links);
     }
 
     /**
